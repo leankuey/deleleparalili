9865b6c7209ae0bb00b0eaa7779f1bceee803bfc
leankuey <2306359@escolas.anchieta.br>
2024-12-03 20:15:55 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,29 +1,9 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
-from MinhasFuncoes import *
+st.title("Bem vinda")
 
-area = AreaTriangulo(3,6)
-st.write("A área do triângulo é: ", area)
+st.header("Se você que esta lendo é Lilian Maria da Silva, esse site é para você")
 
-
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Meu prof. Massaki é D+ e muito convencido!")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Eu te amo meu amor")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-col2, col3 = st.columns(2)
-
-with col2:
-    st.header("NALA")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("CORUJA")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
